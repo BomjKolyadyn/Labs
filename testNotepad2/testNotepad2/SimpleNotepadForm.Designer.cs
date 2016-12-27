3307387b051a90c958dbdd65aae001497a9a150c
Maxim Pravdyuk <pravdyuk.maxim@gmail.com>
2016-12-28 00:47:12 +0300

    MyWordPad

---
+++ testNotepad2/testNotepad2/SimpleNotepadForm.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace testNotepad2
+{
+    partial class SimpleNotepadForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SimpleNotepadForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFileNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFileOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFileSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFileSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuFilePageSetup = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFilePrintPreview = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFilePrint = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuFileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.pageSetupDialog1 = new System.Windows.Forms.PageSetupDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuFile});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(444, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuFile
+            // 
+            this.menuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuFileNew,
+            this.menuFileOpen,
+            this.menuFileSave,
+            this.menuFileSaveAs,
+            this.toolStripSeparator1,
+            this.menuFilePageSetup,
+            this.menuFilePrintPreview,
+            this.menuFilePrint,
+            this.toolStripSeparator2,
+            this.menuFileExit});
+            this.menuFile.Name = "menuFile";
+            this.menuFile.Size = new System.Drawing.Size(37, 20);
+            this.menuFile.Text = "&File";
+            this.menuFile.Click += new System.EventHandler(this.menuFile_Click);
+            // 
+            // menuFileNew
+            // 
+            this.menuFileNew.Name = "menuFileNew";
+            this.menuFileNew.Size = new System.Drawing.Size(143, 22);
+            this.menuFileNew.Text = "&New";
+            this.menuFileNew.Click += new System.EventHandler(this.menuFileNew_Click);
+            // 
+            // menuFileOpen
+            // 
+            this.menuFileOpen.Name = "menuFileOpen";
+            this.menuFileOpen.Size = new System.Drawing.Size(143, 22);
+            this.menuFileOpen.Text = "&Open...";
+            this.menuFileOpen.Click += new System.EventHandler(this.menuFileOpen_Click);
+            // 
+            // menuFileSave
+            // 
+            this.menuFileSave.Name = "menuFileSave";
+            this.menuFileSave.Size = new System.Drawing.Size(143, 22);
+            this.menuFileSave.Text = "&Save";
+            this.menuFileSave.Click += new System.EventHandler(this.menuFileSave_Click);
+            // 
+            // menuFileSaveAs
+            // 
+            this.menuFileSaveAs.Name = "menuFileSaveAs";
+            this.menuFileSaveAs.Size = new System.Drawing.Size(143, 22);
+            this.menuFileSaveAs.Text = "&Save as...";
+            this.menuFileSaveAs.Click += new System.EventHandler(this.menuFileSaveAs_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(140, 6);
+            // 
+            // menuFilePageSetup
+            // 
+            this.menuFilePageSetup.Name = "menuFilePageSetup";
+            this.menuFilePageSetup.Size = new System.Drawing.Size(143, 22);
+            this.menuFilePageSetup.Text = "&Page Setup";
+            this.menuFilePageSetup.Click += new System.EventHandler(this.menuFilePageSetup_Click);
+            // 
+            // menuFilePrintPreview
+            // 
+            this.menuFilePrintPreview.Name = "menuFilePrintPreview";
+            this.menuFilePrintPreview.Size = new System.Drawing.Size(143, 22);
+            this.menuFilePrintPreview.Text = "&Print Preview";
+            this.menuFilePrintPreview.Click += new System.EventHandler(this.menuFilePrintPreview_Click);
+            // 
+            // menuFilePrint
+            // 
+            this.menuFilePrint.Name = "menuFilePrint";
+            this.menuFilePrint.Size = new System.Drawing.Size(143, 22);
+            this.menuFilePrint.Text = "&Print";
+            this.menuFilePrint.Click += new System.EventHandler(this.menuFilePrint_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(140, 6);
+            // 
+            // menuFileExit
+            // 
+            this.menuFileExit.Name = "menuFileExit";
+            this.menuFileExit.Size = new System.Drawing.Size(143, 22);
+            this.menuFileExit.Text = "&Exit";
+            this.menuFileExit.Click += new System.EventHandler(this.menuFileExit_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(444, 265);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "RTF files|*.rtf|Text files|*.txt|All files|*.*";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileName = "doc1.rtf";
+            this.saveFileDialog1.Filter = "RTF files|*.rtf";
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.Document = this.printDocument1;
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Document = this.printDocument1;
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // pageSetupDialog1
+            // 
+            this.pageSetupDialog1.Document = this.printDocument1;
+            // 
+            // SimpleNotepadForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 289);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "SimpleNotepadForm";
+            this.Text = "Notepad";
+            this.Load += new System.EventHandler(this.SimpleNotepadForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuFile;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem menuFileNew;
+        private System.Windows.Forms.ToolStripMenuItem menuFileOpen;
+        private System.Windows.Forms.ToolStripMenuItem menuFileSave;
+        private System.Windows.Forms.ToolStripMenuItem menuFileSaveAs;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Windows.Forms.PageSetupDialog pageSetupDialog1;
+        private System.Windows.Forms.ToolStripMenuItem menuFilePageSetup;
+        private System.Windows.Forms.ToolStripMenuItem menuFilePrintPreview;
+        private System.Windows.Forms.ToolStripMenuItem menuFilePrint;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem menuFileExit;
+    }
+}
+
---
+++ testNotepad2/testNotepad2/SimpleNotepadForm.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.IO; //Для использования потоков класса StringReader;
+using System.Drawing.Printing; //Для работы с компонентами, обеспечивающими возможность печати документов;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testNotepad2
+{
+    public partial class SimpleNotepadForm : Form
+    {
+        /// <summary>
+        /// Инициализая формы программы;
+        /// </summary>
+        public SimpleNotepadForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Кнопка меню File (menuFile);
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void menuFile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Кнопка New из кнопки меню File.(menuFileNew);
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void menuFileNew_Click(object sender, EventArgs e)
+        {
+            if (m_DocumentChanged) //Проверка изменений в документе;
+                MenuFileSaveAs();
+                richTextBox1.Clear();
+        }
+
+        /// <summary>
+        /// Открытие существующего файла;
+        /// Метод для компонента открытия файлов.
+        /// </summary>
+        private void MenuFileOpen()
+        {
+            if (openFileDialog1.ShowDialog() ==   //
+               System.Windows.Forms.DialogResult.OK && //При нажатии "Открыть" файл, возвращает значение DialogResult.OK
+               openFileDialog1.FileName.Length > 0) // И, проверяет, чтобы длина имени файла была больше 0.
+            {
+                try // Попытка открытия файла.
+                {
+                    richTextBox1.LoadFile(openFileDialog1.FileName, //Загрузка файла; Имя файла,
+                       RichTextBoxStreamType.RichText); //Формат файла *rtf.
+                }
+                catch (System.ArgumentException ex) // Ловим исключение если пытаемся открыть файл формата не *rtf.
+                {
+                    richTextBox1.LoadFile(openFileDialog1.FileName, // Загрузка файла; Имя файла,
+                       RichTextBoxStreamType.PlainText); //Формат файла *txt.
+                }
+
+                this.Text = "Файл [" + openFileDialog1.FileName + "]"; //Пишем в шапке "Файл + путь к нему".
+            }
+        }                   
+
+        /// <summary>
+        /// Кнопка Open из кнопки меню File.(menuFileOpen);
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void menuFileOpen_Click(object sender, EventArgs e)
+        {
+            MenuFileOpen();
+        }
+
+        /// <summary>
+        /// Сохранение документа в новом файле; Метод для компонента сохранения файлов.
+        /// У метода saveFileDialog имеется несколько перегруженных вариантов. Можно задать два параметра: путь к файлу(и имя файла соответсвенно); тип файла;
+        /// </summary>
+        private void MenuFileSaveAs()
+        {
+            if (saveFileDialog1.ShowDialog() == //Если мы нажали "сохранить", то происходит сравнение со значением DialogResult.OK;
+               System.Windows.Forms.DialogResult.OK && // Значение DialogResult.OK возвращается;
+            saveFileDialog1.FileName.Length > 0) // Проверка на длину названия файла при сохранении;
+            {
+                richTextBox1.SaveFile(saveFileDialog1.FileName); // Метод для сохранения файлов;
+                m_DocumentChanged = false; // Сброс проверки изменений в документе;
+                this.Text = "Файл [" + saveFileDialog1.FileName + "]"; // Выводит название сохраненного файла в заголовке программы.
+
+            }
+        }
+
+        /// <summary>
+        /// Кнопка Save из кнопки меню File.(menuFileSave);
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void menuFileSave_Click(object sender, EventArgs e)
+        {
+            MenuFileSaveAs(); //Метод сохранения файла;
+        }
+
+        /// <summary>
+        /// Кнопка SaveAs из кнопки меню File.(menuFileSaveAs);
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void menuFileSaveAs_Click(object sender, EventArgs e)
+        {
+            MenuFileSaveAs(); //Метод сохранения файла;
+        }
+
+        /// <summary>
+        /// Настройка параметров страницы; Метод для вывода на экран диалогового окна настройки параметров страницы документа;
+        /// </summary>
+        private void MenuFilePageSetup()
+        {
+            pageSetupDialog1.ShowDialog();
+        }
+
+        /// <summary>
+        /// Кнопка PageSetup из кнопки меню File.(menuFilePageSetup);
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void menuFilePageSetup_Click(object sender, EventArgs e)
+        {
+            MenuFilePageSetup(); //Метод вызова настроек печати страницы;
+        }
+
+        /// <summary>
+        /// StringReader для печати содержимого редактора текста
+        /// </summary>
+        private StringReader m_myReader; //Поле
+
+        /// <summary>
+        /// Номер текущей распечатываемой страницы документа
+        /// </summary>
+        private uint m_PrintPageNumber; //Поле
+
+        /// <summary>
+        /// Предварительный просмотр перед печатью документа
+        /// </summary>
+        private void MenuFilePrintPreview()
+        {
+            m_PrintPageNumber = 1; //Делаем поле равным 1 (т.к. просмотр начнется с первой страницы);
+
+            string strText = this.richTextBox1.Text; //Записывает содержимое файла блокнота в strText;
+            m_myReader = new StringReader(strText); //класс StringReader записыват из strText в поле m_myReader;
+
+            Margins margins = new Margins(100, 50, 50, 50); //Задаются размеры поля печатаемой страницы;
+            printDocument1.DefaultPageSettings.Margins = margins;
+            printPreviewDialog1.ShowDialog(); //Отображает панель предварительного просмотра;
+
+            m_myReader.Close();//Закрытие потока;
+        }
+
+        /// <summary>
+        /// Кнопка Print Preview из кнопки меню File.(menuFilePagePreview); 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void menuFilePrintPreview_Click(object sender, EventArgs e)
+        {
+            MenuFilePrintPreview();//Метод предварительного просмотра печати;
+        }
+
+        /// <summary>
+        /// Печать документа
+        /// </summary>
+        private void MenuFilePrint()
+        {
+            m_PrintPageNumber = 1;//Печать документа начинается с первой страницы;
+
+            string strText = this.richTextBox1.Text; //В переменную strText типа string записывается текст блокнота;
+            m_myReader = new StringReader(strText); //StringReader записывает(strText) в поле m_myReader;
+
+            Margins margins = new Margins(100, 50, 50, 50); // Задается размер окна печати файла;
+            printDocument1.DefaultPageSettings.Margins = margins; //Размер окна становится равным margins;
+
+            if (printDialog1.ShowDialog() == DialogResult.OK) // Если нажимается кнопка "Ок"(печать), то ;
+            {
+                this.printDocument1.Print(); //Документ отправляется на печать методом Print;
+            }
+            m_myReader.Close(); //Поток(поле) m_myReader закрывается (Close);
+        }
+
+        /// <summary>
+        /// Кнопка Print из кнопки меню File.(menuFilePrint);
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void menuFilePrint_Click(object sender, EventArgs e)
+        {
+            MenuFilePrint();
+        }
+
+        private bool m_DocumentChanged = false; //Поле bool для проверки изменений в документе;
+
+        private void menuFileExit_Click(object sender, EventArgs e)
+        {
+            if (m_DocumentChanged)
+                MenuFileSaveAs();
+            this.Close();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            m_DocumentChanged = true; //true - если что-либо написано в документе;
+        }
+
+        private void SimpleNotepadForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
