1cadce4c10c7b19b168575e7b8d333ce09246ed9
Maxim Pravdyuk <pravdyuk.maxim@gmail.com>
2016-12-30 01:12:42 +0300

    wordcount3

---
+++ WordCounter3/WordCounter3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCounter3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int repeatCounter = 0;
+            Console.Write("Введите текст : ");
+            string str = Convert.ToString(Console.ReadLine());
+            string[] words = str.Split(' ');
+            int counter = words.Length;
+            for (int i = 0; i < words.Length; i++)
+                for (int j = 0; j < words.Length; j++)
+                    if ((words[i] == words[j]) && (i != j)) words[i] = null;
+            for (int q = 0; q < words.Length; q++)
+                if (words[q] == null) repeatCounter--;
+            counter = counter - Math.Abs(repeatCounter);
+            foreach (string a in words) Console.WriteLine(a);
+            Console.WriteLine("Counter = {0} , Length = {1} ", counter, words.Length);
+            Console.ReadKey();
+        }
+    }
+}
